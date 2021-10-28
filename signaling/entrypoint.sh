c0cc64931c7d3f175af6bf8f5a5e91894b7610cd
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2021-10-28 19:49:42 -0300

    Add signaling configs

--- signaling/entrypoint.sh
+++ signaling/entrypoint.sh
@@ -5,4 +5,4 @@ sed -i "s|BLOCKKEY|${blockkey}|g" /config/server.conf
 sed -i "s|JANUSKEY|${januskey}|g" /config/server.conf
 sed -i "s|TURNSECRET|${turn_secret}|g" /config/server.conf
 
-signaling
+signaling --config=/config/server.conf
