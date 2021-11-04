83141aba385b1370d436a1147e12f30d8b2c9f2d
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2021-11-04 12:27:57 -0300

    Fix secret name

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -5,7 +5,7 @@ export HASHKEY=$(openssl rand -hex 16)
 sed -i "s|hashkey.*|hashkey=${HASHKEY}|g" ./.env
 
 export BLOCKKEY=$(openssl rand -hex 16) 
-sed -i "s|blockkey.*|blockey=${BLOCKKEY}|g" ./.env
+sed -i "s|blockkey.*|blockkey=${BLOCKKEY}|g" ./.env
 
 export JANUSKEY=$(openssl rand -base64 48) 
 sed -i "s|januskey.*|januskey=${JANUSKEY}|g" ./.env
