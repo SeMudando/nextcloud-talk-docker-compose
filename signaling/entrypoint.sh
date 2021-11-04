af8c2ddf0810cf2551f9ff4b780f522ace609d4e
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2021-11-04 12:44:03 -0300

    Change localhost -> docker host

--- signaling/entrypoint.sh
+++ signaling/entrypoint.sh
@@ -4,5 +4,6 @@ sed -i "s|HASHKEY|${hashkey}|g" /config/server.conf
 sed -i "s|BLOCKKEY|${blockkey}|g" /config/server.conf
 sed -i "s|JANUSKEY|${januskey}|g" /config/server.conf
 sed -i "s|TURNSECRET|${turn_secret}|g" /config/server.conf
+sed -i "s|DOCKER_HOST|${dockerhost}|g" /config/server.conf
 
 signaling --config=/config/server.conf
