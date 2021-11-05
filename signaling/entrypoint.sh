ae7a28355045da57bb81536a3cd91d8a68ec4aab
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2021-11-05 14:47:18 -0300

    Updates server.conf

--- signaling/entrypoint.sh
+++ signaling/entrypoint.sh
@@ -5,5 +5,6 @@ sed -i "s|BLOCKKEY|${blockkey}|g" /config/server.conf
 sed -i "s|JANUSKEY|${januskey}|g" /config/server.conf
 sed -i "s|TURNSECRET|${turn_secret}|g" /config/server.conf
 sed -i "s|DOCKER_HOST|${dockerhost}|g" /config/server.conf
+sed -i "s|CLOUDDOMAIN|${cloud_domain}|g" /config/server.conf
 
 signaling --config=/config/server.conf
